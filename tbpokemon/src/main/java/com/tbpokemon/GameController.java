5e3732fa317d3b76dfa3814c05704b53c9ebc363
TheInfiniteTrooper <theinfinitetrooper@gmail.com>
2025-05-10 19:24:46 -0400

    Added Weavile

--- tbpokemon/src/main/java/com/tbpokemon/GameController.java
+++ tbpokemon/src/main/java/com/tbpokemon/GameController.java
@@ -136,7 +136,7 @@ public class GameController {
     private void choosePokemon() {
         Integer opt;
         while (true) {
-            System.out.println("Choose your Pokemon:\n1) Squirtle 2) Bulbasaur 3) Charmander 4) Gengar 5) Pikachu");
+            System.out.println("Choose your Pokemon:\n1) Squirtle 2) Bulbasaur 3) Charmander 4) Gengar 5) Pikachu 6) Weavile");
             try {
                 opt = getOption();
                 this.playerPokemon = pokemonSelector(opt);
@@ -151,7 +151,7 @@ public class GameController {
     
     private Pokemon pokemonSelector(Integer opt) {
         if (opt == null) {
-            throw new IllegalArgumentException("Option should be 1, 2, 3, 4 or 5!");
+            throw new IllegalArgumentException("Option should be 1, 2, 3, 4, 5 or 6!");
         }
         switch (opt) {
             case 1:
@@ -164,13 +164,15 @@ public class GameController {
                 return pokedex.getPokemon("gengar").clone();
             case 5:
                 return pokedex.getPokemon("pikachu").clone();
+            case 6:
+                return pokedex.getPokemon("weavile").clone();
             default:
-                throw new IllegalArgumentException("Option should be 1, 2, 3, 4 or 5!");
+                throw new IllegalArgumentException("Option should be 1, 2, 3, 4, 5 or 6!");
         }
     }
 
     private void spawnWildPokemon() {
-        int num = (int)(Math.random() * 5) + 1;
+        int num = (int)(Math.random() * 6) + 1;
         this.enemyPokemon = pokemonSelector(num);
         this.enemyPokemon.resetHP();
         System.out.println("You venture into the tall grass.");
